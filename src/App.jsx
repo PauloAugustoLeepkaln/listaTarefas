aea63781ae0e0009d04c769c129ac51799067842
Paulo-Augusto <paulo70306@edu.unipar.br>
2025-09-15 20:41:12 -0300

    commit?

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,45 @@ function App() {
   const [tarefa, setTarefa] = useState("");
   const [listaTarefas, setListaTarefas] = useState([]);
 
-  const adicionarTarefa = () => {
+  useEffect(()=>{
 
-    if(tarefa.trim() === ''){
-      alert("Informe uma tarefa")
-      return
+        localStorage.setItem("LISTA_TAREFA", JSON.stringify(listaTarefas))
+
+  } , [listaTarefas])
+
+  const adicionarTarefa = () => {
+    if (tarefa.trim() === "") {
+      alert("Informe uma tarefa");
+      return;
     }
 
     const novaTarefa = {
-      id : Date.now(),
+      id: Date.now(),
       nome: tarefa,
+      completada: false,
     };
     setListaTarefas([...listaTarefas, novaTarefa]);
 
+    setTarefa("");
     console.log(listaTarefas);
+  };
+
+  const alterarSituacao = (id) => {
+    setListaTarefas(
+      listaTarefas.map((task) =>
+        task.id == id ? { ...task, completada: !task.completada } : task
+      )
+    );
+  };
+
+  const excluirTarefa = (id) => {
+    setListaTarefas(listaTarefas.filter((task) => task.id != id));
   };
 
   return (
     <div className="todo-container">
       <h2>Lista de Tarefas ✅</h2>
+      <p> Total tarefas : { listaTarefas.length || "Sem Tarefas"}</p>
       <div className="input-container">
         <input
           type="text"
@@ -36,11 +56,18 @@ function App() {
 
       <ul>
         {listaTarefas.map((task) => (
-          <li key={task.id}>
+          <li
+            key={task.id}
+            style={{
+              textDecoration: task.completada ? "line-through" : "none",
+              color: task.completada ? "gray" : "black",
+            }}
+          >
+          
             {task.nome}
             <div>
-              <button className="complete-btn">✔</button>
-              <button className="delete-btn">❌</button>
+              <button onClick={() => alterarSituacao(task.id)} className="complete-btn">✔</button>
+              <button onClick={() => excluirTarefa(task.id)} className="delete-btn">❌</button>
             </div>
           </li>
         ))}
